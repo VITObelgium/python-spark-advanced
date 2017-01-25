b2954ea51f3b96c4849a70902f0fd868adfd1a59
Jan Van den bosch <jan.van.den.bosch@devoteam.com>
2017-01-25 14:19:34 +0100

    replaced file removal with modification date check

--- test-cluster.sh
+++ test-cluster.sh
@@ -1,14 +1,42 @@
 #!/bin/sh
 
-OUTPUT_DIR=/tmp_epod/tmp
+output_dir=/tmp_epod/tmp
 
 if [ "$#" -gt 0 ]; then
-  OUTPUT_DIR=$1
+  output_dir=$1
 fi
 
-rm -f $OUTPUT_DIR/subtile_average_17_2.tif && \
-rm -f $OUTPUT_DIR/subtile_average_18_2.tif && \
-sh run-cluster $OUTPUT_DIR && \
-echo "d18b61eec14e203157961571b84b795b  $OUTPUT_DIR/subtile_average_17_2.tif" | md5sum -c - && \
-echo "39978405e412a2e0cf09c5ad6193c1d0  $OUTPUT_DIR/subtile_average_18_2.tif" | md5sum -c -
+last_update() {
+  local file=$1
+  echo $(stat -c %Y $file)
+}
+
+current_time() {
+  echo $(date +%s)
+}
+
+fail_if_too_old() {
+  local file=$1
+  local max_secs=$2
+
+  local file_changed_at=$(last_update $file1)
+  local now=$(current_time)
+  local diff=$((now - file_changed_at))
+
+  if [ $diff -gt $max_secs ]; then
+    echo "$file has not been written to recently - did the job run?"
+    exit 1
+  fi
+}
+
+sh run-cluster $output_dir
+
+file1="$output_dir/subtile_average_17_2.tif"
+file2="$output_dir/subtile_average_18_2.tif"
+
+fail_if_too_old $file1 120
+fail_if_too_old $file2 120
+
+echo "d18b61eec14e203157961571b84b795b  $file1" | md5sum -c - && \
+echo "39978405e412a2e0cf09c5ad6193c1d0  $file2" | md5sum -c -
 
